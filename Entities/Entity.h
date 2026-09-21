cf634dfb63373cc9e1256e79b30388efde2e86ce
NamKhanhCoder <namkhanhcoder@gmail.com>
2026-09-22 00:03:57 +0700

    Hoàn thành input helper, validator

---
+++ Entities/Entity.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <string>
+#include <vector>
+#include "Utils/Validator.h"
+using namespace std;
+
+class Entity
+{
+protected:
+    string id;
+
+public:
+    Entity()
+    {
+    }
+    Entity(string id)
+    {
+        this->id = id;
+    }
+    virtual ~Entity() {}
+
+    string getID() const // promise to compiler that this func will not change variables, because when passing a constant address like const Entity& e to a function then call e.getID() cpp feared that the func might change the value of a constant variable.
+    {
+        return id;
+    }
+    virtual void setID(const string &ID) = 0;
+    virtual void parseFromString(const string &data) = 0;
+
+    virtual string exportToString() const=0;
+    virtual vector<string> exportToVector() const = 0;
+};
--- main.cpp
+++ main.cpp
@@ -12,10 +12,20 @@ void testValidator(string inp)
     Validator::isValidPhone(inp);
     Validator::isValidEmail(inp);
 }
+void testInputHelper()
+{
+    string inp;
+    inp = InputHelper::getInputInt("Enter some int:");
+    inp = InputHelper::getInputDouble("Enter some double:");
+    inp = InputHelper::getInputString("Enter some string (non empty)");
+    inp = InputHelper::getInputOptionalString("Enter some string");
+    inp = InputHelper::getInputEmail("Enter some email:");
+    inp = InputHelper::getInputPhone("Enter some phon:");
+}
 int main()
 {
     while (1)
     {
-        //test
+        // test
     }
 }
